8617cd3e3d5cdfd72dcfb3afcfa1d46b24f07196
Jaroslav Langer <langer.jaros@gmail.com>
2022-04-17 01:39:54 +0200

    parallel_data.c implementation of OpenMP data parallelism, test.sh was improved.

--- test.sh
+++ test.sh
@@ -2,10 +2,9 @@
 # USAGE: ./test.sh GRAPHS SOLUTIONS SOLVER
 PATTERN="${1%/*}/graf_[0-9]+_[0-9]+\.txt"
 find $1 -regex $PATTERN | while read path; do
-    echo $(date --iso-8601=ns) $path
-    $3 $path > tmp.txt
+    time_out=$(/usr/bin/time -f '%e (%U ~ %P)' $3 $path 2>&1 > tmp.txt)
+    echo "$(date --iso-8601=ns) | $path | $time_out"
     name=$(basename $path)
     diff ${2%/*}/${name} tmp.txt
 done
-date --iso-8601=ns
 rm tmp.txt
